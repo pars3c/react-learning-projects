a7e8b32758cf243a065fb9225e996be97e83d64c
aderito.xavier <aderito_xavier@sapo.pt>
2018-10-10 11:33:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+function Cars(data){
+  return "The brand of the car is: " + data.brand + " and the model is: " + data.model;
+};
+
+const data = {
+  brand: 'Fiat',
+  model: 'Punto',
+};
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, wsorld!'
+);
+
+
+class Hello extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      element
     );
   }
 }
 
-export default App;
+export default Hello;
