5bb4d3a3a92c74f09157852672b705a8c366e455
aderito xavier <aderito_xavier@sapo.pt>
2018-11-11 19:17:58 +0000

    src folder removed and proped for another project

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap'; 
+import './App.css'
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            newDate: '',
+        }
+    }
+    changeBirthday() {
+        console.log(this.state);
+    }
+    render(){
+        return (
+            
+            <div className="App">
+                <Form inline>
+                    <h2>Please type in your birthday</h2>
+                    <FormControl 
+                    type='date'
+                    onChange={ event => this.setState({ newDate: event.target.value }) }
+                    ></FormControl>
+                    {' '}
+                    <Button
+                    onClick={() => this.changeBirthday()}
+                    >Submit</Button>
+                </Form>
+            </div>
+
+        )
+
+    }
+
+}
+
+export default App;
